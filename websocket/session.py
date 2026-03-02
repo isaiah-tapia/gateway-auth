bcd77a6e4edce1b5520513c4fd949ab5672bee85
isaiah-tapia <isaiahtapia@berkeley.edu>
2026-03-02 14:57:59 -0800

    session

--- websocket/session.py
+++ websocket/session.py
@@ -5,12 +5,20 @@ import time
 import uuid
 from collections import defaultdict, deque
 from datetime import datetime, timedelta
+from datetime import timezone 
 from typing import Dict, Optional, Set
 
 import jwt
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+
+SIGNKEY = "foo"
+ALGO = "HS256"
+TOKEN_EXPIRY = 15
+RATE_LIMIT_SESSION = 10
+RATE_LIMIT_WINDOW = 60
+MAX_MISSED = 100
 
 class Session():
     """
@@ -21,14 +29,33 @@ class Session():
         self.session_id = session_id
         self.user_id = user_id
         self.channel = channel
-        self.connected_at = datetime.utcnow()
+        self.connected_at =  datetime.now(timezone.utc).isoformat()
         self.websocket: Optional[WebSocket] = None
-        self.missed_messages: deque = deque(maxlen=MAX_MISSED_MESSAGES)
-        # Rate limiting
+        self.missed_messages: deque = deque(maxlen=MAX_MISSED)
         self.message_timestamps: deque = deque()
 
 class SessionStore():
     """
-    We need to keep track of the session which are on going, this is our trusted computing base. lets assume 
-    that this cannot be penetrated
+    We need to keep track of the session which are on going, this is our trusted computing base.
     """
+
+    def __init__(self):
+        # maps a user to their session id
+        self._sessions: Dict[str, Session] = {}
+        pass
+
+    def create(self, ):
+        pass
+
+    def get(self):
+        pass
+    
+    def get_session_by_user(self):
+        pass
+
+    def attach_websocket(self):
+        pass
+
+    def detach_websocket(self):
+        pass
+
