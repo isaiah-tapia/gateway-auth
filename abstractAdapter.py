4b1e233ecc472b36cba1668a5531c9a91ef228e2
isaiah-tapia <isaiahtapia@berkeley.edu>
2026-03-02 13:19:03 -0800

    abstract adapter

---
+++ abstractAdapter.py
@@ -0,0 +1,37 @@
+import uuid
+import encodings
+from datetime import datetime
+from abc import ABC, abstractmethod
+
+class abstractAdapter():
+
+    @abstractmethod
+    def normalize(self, dict):
+        """
+        This method should be implemented across our kinds of channels
+        """
+        pass
+
+    @abstractmethod
+    def outgoing_denormalize(self, dict):
+        """
+        Denomralize our response and send it back to our client
+        """
+        pass
+    
+    def abs_scheme(self, session_id: str, user_id: str, text: str,
+                   attachments: list = None, msg_id: str = None):
+        """
+        base scheme strucure. Should stay the same across our adapters.  
+        """
+        return {
+            "id": msg_id or str(uuid.uuid4()),
+            "session_id": session_id,
+            "channel": self.channel,
+            "user_id": user_id,
+            "text": text,
+            "timestamp": datetime.utcnow().isoformat(),
+            "attachments": attachments or []
+        }
+
+
